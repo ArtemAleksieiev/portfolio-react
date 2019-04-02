1a494a65da23b9a607a25b95f368b933b89b03da
Artem Aleksieiev <artemaleksieiev@Artems-iMac.home>
2019-04-02 16:19:27 -0400

    projects page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
 
 class Landing extends Component {
   render() {
     return(
-      <div><h1>Landing Page</h1></div>
+      <div className="landing" style={{width: '100%', height: '100%', margin: 'auto'}}>
+      <Grid className="landing-grid">
+      <Cell col={12}>
+      <img
+          src={require("../img/image.png")}
+          alt ="avatar"
+          className="avatar-img"
+      />
+      <div className="banner-text">
+      <h1>Artem Aleksieiev</h1>
+      <hr/>
+      <p>HTML/CSS | Bootstrap | Javascript | React | Python | Swift </p>
+
+      <div className="social-links">
+
+      {/* LinkedIn */}
+      <a href="http://google.com" rel="noopener noreferrer" targer="_blank">
+        <i className="fa fa-linkedin-square" aria-hidden="true" />
+      </a>
+
+      {/* Github */}
+      <a href="http://google.com" rel="noopener noreferrer" targer="_blank">
+        <i className="fa fa-github-square" aria-hidden="true" />
+      </a>
+
+      {/* Facebook */}
+      <a href="http://google.com" rel="noopener noreferrer" targer="_blank">
+        <i className="fa fa-facebook-square" aria-hidden="true" />
+      </a>
+
+      {/* Instagram */}
+      <a href="http://google.com" rel="noopener noreferrer" targer="_blank">
+        <i className="fa fa-instagram" aria-hidden="true" />
+      </a>
+      </div>
+      </div>
+      </Cell>
+      </Grid>
+
+      </div>
     )
   }
 }
