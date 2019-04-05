d2cec33f0756aafb637722cf8a17094c8ce2c791
Artem Aleksieiev <artemaleksieiev@Artems-iMac.home>
2019-04-04 20:57:18 -0400

    Updates

---
+++ precache-manifest.ab4a6b832702a3579945f2150bd09f59.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "184eb04a459843a7e203e2e00c9476ca",
+    "url": "/portfolio-react/static/media/image.184eb04a.png"
+  },
+  {
+    "revision": "9f6dbd95aa3740e0b745",
+    "url": "/portfolio-react/static/css/main.89ec7461.chunk.css"
+  },
+  {
+    "revision": "11528ddda4154ce1e32b",
+    "url": "/portfolio-react/static/js/runtime~main.72b3206a.js"
+  },
+  {
+    "revision": "2fe053304f5aad7abe5ed29353a3d961",
+    "url": "/portfolio-react/static/media/skype-brands.2fe05330.svg"
+  },
+  {
+    "revision": "9ebc63b002d35d8fc8bb",
+    "url": "/portfolio-react/static/js/2.306338b1.chunk.js"
+  },
+  {
+    "revision": "2a191a99d492c8bd3d11bf34ee16a76c",
+    "url": "/portfolio-react/static/media/nyc.2a191a99.jpg"
+  },
+  {
+    "revision": "9f6dbd95aa3740e0b745",
+    "url": "/portfolio-react/static/js/main.54e34ebf.chunk.js"
+  },
+  {
+    "revision": "9151a48ab8cc21ea2829a0a534ced8ea",
+    "url": "/portfolio-react/static/media/photo.9151a48a.jpg"
+  },
+  {
+    "revision": "d979d27767ef08c8ef381f39b0c84490",
+    "url": "/portfolio-react/static/media/phone-square-solid.d979d277.svg"
+  },
+  {
+    "revision": "93e1a8bd859b12bf2d7fc51f8b81241f",
+    "url": "/portfolio-react/static/media/envelope-square-solid.93e1a8bd.svg"
+  },
+  {
+    "revision": "9ebc63b002d35d8fc8bb",
+    "url": "/portfolio-react/static/css/2.7b11ba96.chunk.css"
+  },
+  {
+    "revision": "757d21c022e1569e09d2b60ad48dd889",
+    "url": "/portfolio-react/index.html"
+  }
+];
